53c01644c8f7cbe39705082854a38df326e09555
kt <140302003@svuca.edu>
2019-09-18 19:07:51 -0700

    assignmnet-testing completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -17,4 +17,10 @@ describe('MenuController', () => {
             expect(this.menu.getContactCount()).toBe(1);
         });
     });
+
+    describe('#remindMe', () => {
+        it('should return a string containing the text "Learning is a life-long pursuit"', () => {
+            expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit');
+        });
+    });
 });
