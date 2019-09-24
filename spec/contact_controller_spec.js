d612fb102321ef92209e7e59243ab252b7cdef3d
ktisus <140302003@svuca.edu>
2019-09-24 11:37:53 -0700

    assignment_createContacts_connectDatabase completed

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -24,11 +24,12 @@ describe("ContactController", () => {
 // #1
         it("should add a single contact into the book", (done) => {
 // #2     
-            this.book.addContact("Alice", "001-101-1010")
+            this.book.addContact("Alice", "001-101-1010", "a@gmail.com")
             .then((contact) => {
 // #3
                 expect(contact.name).toBe("Alice");
                 expect(contact.phone).toBe("001-101-1010");
+                expect(contact.email).toBe("a@gmail.com")
                 done();
               })
               .catch((err) => {
